9252f5b72eebeae84aaa4418eea8af6c7f743fdb
Joel <JoelBarr1220@gmail.com>
2017-01-20 23:40:56 -0700

    Created UI and WaveManager class

---
+++ Assets/Scripts/PurchaseButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchaseButton : MonoBehaviour {
+
+    Transform popup;
+
+	// Use this for initialization
+	void Start () {
+        popup = transform.FindChild("Popup Panel");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void OnMouseEnter()
+    {
+        print("Mouseover");
+        popup.gameObject.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        print("Mouse Exit");
+        popup.gameObject.SetActive(false);
+    }
+
+    public void Buy(string itemName)
+    {
+        print("Purchased" + itemName);
+    }
+}
