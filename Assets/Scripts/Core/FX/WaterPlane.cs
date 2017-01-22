046e28dcd893c687480c6fda7c07be27990d90b7
timmypowergamer@bitbucket.org <timmypowergamer@gmail.com>
2017-01-22 14:16:31 -0700

    water stuff

--- Assets/Scripts/Core/FX/WaterPlane.cs
+++ Assets/Scripts/Core/FX/WaterPlane.cs
@@ -10,12 +10,41 @@ public class WaterPlane : MonoBehaviour {
     [SerializeField]
     private Color _color;
 
+    bool updateTexture = true;
+
+    public SpriteRenderer Sprite
+    {
+        get
+        {
+            if(_sprite == null)
+            {
+                _sprite = GetComponent<SpriteRenderer>();
+            }
+            return _sprite;
+        }
+    }
+
+    void Awake()
+    {
+        transform.rotation = Quaternion.LookRotation(Vector3.up);
+    }
+
     public void SetFill(float fillAmount)
     {
         _color.a = fillAmount;
-        _sprite.color = _color;
+        Sprite.color = _color;
 
         if (_color.a > 0 && !gameObject.activeSelf) gameObject.SetActive(true);
         else if (_color.a == 0 && gameObject.activeSelf) gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (updateTexture)
+        {
+            Vector2 offset = Sprite.material.mainTextureOffset;
+            offset.y += 1f * Time.deltaTime;
+            Sprite.material.SetTextureOffset("_MainTex", offset);
+        }
     }
 }
