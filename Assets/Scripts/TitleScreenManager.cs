82f1bfe2f2914cdc6868662ccf141f0b8893b0a8
Joel <JoelBarr1220@gmail.com>
2017-01-22 15:05:59 -0700

    Title screen touched up. Music, ambiance, transition added

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScreenManager : MonoBehaviour {
+
+    public AudioSource music, ambiance, transition;
+    public Animation fadeToBlack;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +15,18 @@ public class TitleScreenManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Space) && !IsInvoking("LoadNext"))
+        {
+            Invoke("LoadNext", 5.25f);
+            fadeToBlack.Play();
+            music.Stop();
+            ambiance.Stop();
+            transition.Play();
+        }
 	}
+
+    void LoadNext()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
 }
